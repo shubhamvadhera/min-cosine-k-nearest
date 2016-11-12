f6fc072ea5e7998b977369213cbdaa3a12d7fff8
shubhamvadhera@gmail.com <shubhamvadhera@gmail.com>
2016-11-12 02:13:56 -0800

    f

---
+++ gol.go
@@ -0,0 +1,347 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
+
+/* Tuple for document number pairs */
+type DocPair struct {
+	doc1 int
+	doc2 int
+}
+
+/* Structs and functions related to sorting a map:
+* adopted as is from https://gist.github.com/ikbear/4038654
+ */
+
+type sortedMap struct {
+	m map[DocPair]float64
+	s []DocPair
+}
+
+func (sm *sortedMap) Len() int {
+	return len(sm.m)
+}
+
+func (sm *sortedMap) Less(i, j int) bool {
+	return sm.m[sm.s[i]] > sm.m[sm.s[j]]
+}
+
+func (sm *sortedMap) Swap(i, j int) {
+	sm.s[i], sm.s[j] = sm.s[j], sm.s[i]
+}
+
+func sortedKeys(m map[DocPair]float64) []DocPair {
+	sm := new(sortedMap)
+	sm.m = m
+	sm.s = make([]DocPair, len(m))
+	i := 0
+	for key, _ := range m {
+		sm.s[i] = key
+		i++
+	}
+	sort.Sort(sm)
+	return sm.s
+}
+
+/* End of sort functions and structs */
+
+/* Global Variables */
+var records int
+var ptr []int
+var ind []int
+var val []int
+var numCosineSim int
+var cosims map[DocPair]float64
+var normValues map[int]float64
+var normVectors map[int]map[int]float64
+var nbrslist []int
+
+/* Function to print file statistics like number of rows */
+func printFileStats(filename string) {
+	if file, err := os.Open(filename); err == nil {
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		numLines := 0
+		maxCol := 0
+		maxi := 0
+		for scanner.Scan() {
+			numLines = numLines + 1
+			line := scanner.Text()
+			arr := strings.Split(line, " ")
+			for _, str := range arr {
+				if maxCol < len(arr) {
+					maxCol = len(arr)
+				}
+				if i, _ := strconv.Atoi(str); i > maxi {
+					maxi, _ = strconv.Atoi(str)
+				}
+			}
+		}
+		fmt.Println("number of lines: ", numLines)
+		fmt.Println("max Value: ", maxi)
+		fmt.Println("max Columns: ", maxCol)
+		if err = scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Fatal(err)
+	}
+}
+
+/* Reads the file and creates ptr, ind, val slices */
+func readCreateCSR(filename string) {
+	if file, err := os.Open(filename); err == nil {
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		ptr = append(ptr, 0)
+		for scanner.Scan() {
+			records += 1
+			arr := strings.Split(strings.Trim(scanner.Text(), " "), " ")
+			//fmt.Println(arr)
+			l := len(arr)
+			//println(l/2)
+			ptr = append(ptr, ptr[len(ptr)-1]+l/2)
+			for i := 0; i < l; i++ {
+				x, _ := strconv.Atoi(arr[i])
+				if i%2 == 0 {
+					ind = append(ind, x)
+				} else {
+					val = append(val, x)
+				}
+			}
+		}
+		//fmt.Println(ptr)
+		//fmt.Println(ind)
+		//fmt.Println(val)
+		fmt.Println("Number of records: ", records)
+		if err = scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Fatal(err)
+	}
+}
+
+/* Calculates Cosine Similarity by dot product
+* algorithm adopted from findsim by Prof. David C. Anastasiu
+ */
+func cosineSimDot(docNum1 int, docNum2 int) float64 {
+	numCosineSim += 1
+	if docNum1 == docNum2 {
+		return 1.0
+	}
+	// caching of cosine similarities -> (disabled since no signficant performance gain)
+	/*docPair := DocPair{doc1: docNum1, doc2: docNum2}
+	if v, ok := cosims[docPair]; ok {
+		return v
+	}
+	docPairInv := DocPair{doc1: docNum2, doc2: docNum1}
+	if v, ok := cosims[docPairInv]; ok {
+		return v
+	}*/
+
+	fr1 := ptr[docNum1-1]
+	to1 := ptr[docNum1]
+	fr2 := ptr[docNum2-1]
+	to2 := ptr[docNum2]
+	i := fr1
+	j := fr2
+	dp := 0.0
+	normDoc1 := 0.0
+	normDoc2 := 0.0
+	simi := 0.0
+	for i < to1 && j < to2 {
+		if i == to1 { //i reaches end
+			normDoc2 = normDoc2 + float64(val[j]*val[j])
+			j += 1
+		} else if j == to2 { // j reaches end
+			normDoc1 = normDoc1 + float64(val[i]*val[i])
+			i += 1
+		} else if ind[i] > ind[j] {
+			normDoc2 = normDoc2 + float64(val[j]*val[j])
+			j += 1
+		} else if ind[i] < ind[j] {
+			normDoc1 = normDoc1 + float64(val[i]*val[i])
+			i += 1
+		} else {
+			dp = dp + float64(val[i]*val[j])
+			normDoc1 = normDoc1 + float64(val[i]*val[i])
+			normDoc2 = normDoc2 + float64(val[j]*val[j])
+			i += 1
+			j += 1
+		}
+	}
+	if dp > 0.0 {
+		simi = dp / math.Sqrt(normDoc1*normDoc2)
+	}
+	/*cosims[docPair] = simi*/
+	// docPair = DocPair{doc1: docNum2, doc2:docNum1}
+	// cosims[docPair] = simi
+	return simi
+}
+
+/* Calculates and caches norm of a document */
+func normValue(docnumber int) float64 {
+	if v, ok := normValues[docnumber]; ok {
+		return v
+	}
+	fr := ptr[docnumber-1]
+	to := ptr[docnumber]
+	nval := 0.0
+	for i := fr; i < to; i++ {
+		nval += float64(val[i] * val[i])
+	}
+	nval = math.Sqrt(nval)
+	normValues[docnumber] = nval
+	return nval
+}
+
+/* Calculates and caches norm vector of a document */
+func normVector(docnumber int) map[int]float64 {
+	if v, ok := normVectors[docnumber]; ok {
+		return v
+	}
+	fr := ptr[docnumber-1]
+	to := ptr[docnumber]
+	dic := make(map[int]float64)
+	for i := fr; i < to; i++ {
+		dic[ind[i]] = float64(val[i]) / normValue(docnumber)
+	}
+	normVectors[docnumber] = dic
+	return dic
+
+}
+
+/* Calculates Cosine Similarity by normalized vectors
+* slower than cosineSimDot in Golang
+ */
+func cosineSimNorm(docNum1 int, docNum2 int) float64 {
+	numCosineSim += 1
+	if docNum1 == docNum2 {
+		return 1.0
+	}
+	docPair := DocPair{doc1: docNum1, doc2: docNum2}
+	if v, ok := cosims[docPair]; ok {
+		return v
+	}
+	docPairInv := DocPair{doc1: docNum2, doc2: docNum1}
+	if v, ok := cosims[docPairInv]; ok {
+		return v
+	}
+	doc1norm := normVector(docNum1)
+	doc2norm := normVector(docNum2)
+	simi := 0.0
+	for k, _ := range doc1norm {
+		if _, ok := doc2norm[k]; ok {
+			simi += doc1norm[k] * doc2norm[k]
+		}
+	}
+	cosims[docPair] = simi
+	return simi
+}
+
+/* Finds the probable neighbours as per IdxJoin */
+func knnIdx() {
+	nbrs := make(map[int]bool)
+
+	for docnum := 1; docnum < records+1; docnum++ {
+		if _, ok := nbrs[docnum]; ok {
+			//print("nbr hit")
+			continue
+		}
+		fr := ptr[docnum-1]
+		to := ptr[docnum]
+		l := len(ptr)
+		for d := fr; d < to; d++ {
+			for x := 1; x < l; x++ {
+				if _, ok := nbrs[x]; ok {
+					continue
+				}
+				n := ptr[x-1]
+				if n == fr {
+					continue
+				}
+				for ind[n] <= ind[d] && n < ptr[x] {
+					if ind[n] == ind[d] {
+						nbrs[x] = true
+						nbrs[docnum] = true
+						break
+					}
+					n += 1
+				}
+			}
+		}
+	}
+
+	for k := range nbrs {
+		nbrslist = append(nbrslist, k)
+	}
+	println("Neighbours created")
+}
+
+/* Finds similarity within probable neighbours */
+func knn(eps float64, k int) {
+	klist := make(map[DocPair]float64)
+	//fmt.Println("nbrslist: ", nbrslist)
+	l := len(nbrslist)
+	println("Number of probable neighbours: ", l)
+	for i := 0; i < l; i++ {
+		for j := i + 1; j < l; j++ {
+			simi := cosineSimDot(nbrslist[i], nbrslist[j])
+			if simi >= eps {
+				docPair := DocPair{doc1: nbrslist[i], doc2: nbrslist[j]}
+				klist[docPair] = simi
+			}
+		}
+	}
+	println("Number of computed similarities: ", numCosineSim)
+	if len(klist) < k {
+		k = len(klist)
+	}
+	fmt.Println("klist: ", sortedKeys(klist)[:k])
+}
+
+func knnAll() {
+	knnIdx()
+}
+
+func main() {
+	println("********************************************************************************")
+	println("findsim-golang (0.0.1), vInfo: [initial version]")
+	println("mode: ij, iFile: , oFile: , k: , eps: ")
+	println("********************************************************************************")
+	tStart := time.Now()
+	//printFileStats("wiki10.csr")
+	readCreateCSR("wiki10.csr")
+	knnIdx()
+	//cosims = make(map[DocPair]float64)
+	//normValues = make(map[int]float64)
+	//normVectors = make(map[int]map[int]float64)
+	// for i := 1; i <= records; i++ {
+	// 	println(i)
+	// 	for j := i; j <= records; j++ {
+	// 		cosineSimDot(i, j)
+	// 	}
+	// }
+	//knnIdx()
+	knn(0.4, 10)
+	//fmt.Println(len(nbrs))
+	//fmt.Println(cosims)
+	//fmt.Println(cosineSimDot(258,259))
+	//fmt.Println(cosineSimNorm(1,2))
+	// fmt.Println(normValue(3))
+	// fmt.Println(normValue(3))
+	// fmt.Println(normVector(1))
+	// fmt.Println(normVector(1))
+
+	fmt.Println("Program time: ", time.Since(tStart))
+}
---
+++ prog.py
@@ -0,0 +1,252 @@
+import time
+startTime = time.time()
+import math
+
+#GLOBAL VARIABLES
+records=0
+ptr=[]
+ind=[]
+val=[]
+normValues={}
+normVectors={}
+cosims={}
+nbrs = set()
+numCosineSim=0
+
+#def initialize():
+
+def printFileStats(filename):
+	numLines=0
+	maxi=0
+	maxCol=0
+	with open(filename) as lines:
+		for line in lines:
+			numLines=numLines+1
+			arr = line.split()
+			for x in arr:
+				if maxCol < len(arr):
+					maxCol = len(arr)
+				if int(x) > maxi:
+					maxi = int(x)
+	print("number of lines: " + str(numLines))
+	print("max Value: " + str(maxi))
+	print("max Columns: " + str(maxCol))
+
+def readCSR(filename):
+	global records
+	global cosims
+	docs=[]
+	
+	#docs.append({})
+	with open(filename) as lines:
+		for line in lines:
+			records = records + 1
+			arr = line.split()
+			i = 0
+			l = len(arr)
+			#print(arr)
+			dic = {}
+			while (i < l):
+				dic[ int(arr[i]) ] = int(arr[i+1])
+				i = i + 2
+			docs.append(dic)
+	#cosims = [[-1.0]*records for _ in range(records)] #cosine similarities between same elements is 1
+	#print(cosims)
+	#print(len(cosims))
+	#print(len(cosims[0]))
+	return docs
+
+def createCSR(filename):
+	global ptr
+	global records
+	global ind
+	global val
+	global norm
+
+	docs = readCSR(filename)
+	
+	ptr.append(0)
+	#print(docs)
+	i=0
+	#Creating ptr, ind
+	for i in range(0,records):
+		ptr.append(ptr[i] + len(docs[i]))
+		for key in sorted(docs[i]):
+			ind.append(key)
+			val.append(docs[i][key])
+	#print(ptr)
+	#print(ind)
+	#print(val)
+
+def normValue(docnumber):
+	global normValues
+	if docnumber in normValues:
+		return normValues[docnumber]
+	global ptr
+	global val
+	fr = ptr[docnumber-1]
+	to = ptr[docnumber]
+	nval = 0.0
+	for i in range(fr,to):
+		nval = nval + val[i]**2
+	nval = math.sqrt(nval)
+	normValues[docnumber] = nval
+	return nval
+
+def normVector(docnumber):
+	global normVectors
+	if docnumber in normVectors:
+		return normVectors[docnumber]
+	global ptr
+	global ind
+	global val
+	fr = ptr[docnumber-1]
+	to = ptr[docnumber]
+	dic = {}
+	for i in range (fr,to):
+		dic[ind[i]] = val[i] / normValue(docnumber)
+	normVectors[docnumber] = dic
+	return dic
+
+def cosineSimNorm(docNum1, docNum2):
+	global numCosineSim
+	numCosineSim = numCosineSim + 1
+	if docNum1 == docNum2:
+		return 1.0
+	global cosims
+	if (docNum1, docNum2) in cosims or (docNum2, docNum1) in cosims:
+		return cosims[(docNum1,docNum2)]
+	
+	doc1norm = normVector(docNum1)
+	doc2norm = normVector(docNum2)
+
+	simi = 0.0
+
+	for k,v in doc1norm.items():
+		if k in doc2norm:
+			simi = simi + doc1norm[k]*doc2norm[k]
+	
+	cosims[(docNum1,docNum2)] = simi
+	cosims[(docNum2,docNum1)] = simi
+	return simi
+
+def cosineSimDot(docNum1,docNum2): #slower
+	global numCosineSim
+	numCosineSim = numCosineSim + 1
+	if docNum1 == docNum2:
+		return 1.0
+	global cosims
+	if (docNum1, docNum2) in cosims or (docNum2, docNum1) in cosims:
+		return cosims[(docNum1,docNum2)]
+
+	global ptr
+	global ind
+	global val
+
+	fr1 = ptr[docNum1-1]
+	to1 = ptr[docNum1]
+	fr2 = ptr[docNum2-1]
+	to2 = ptr[docNum2]
+
+	i=fr1
+	j=fr2
+	dp = 0.0
+	normDoc1 = 0.0
+	normDoc2 = 0.0
+	while(i<to1 and j<to2):
+		if i == to1: #i reaches end
+			normDoc2 = normDoc2 + val[j]**2
+			j=j+1
+		elif j == to2: # j reaches end
+			normDoc1 = normDoc1 + val[i]**2
+			i=i+1
+		elif ind[i] > ind[j]:
+			normDoc2 = normDoc2 + val[j]**2
+			j=j+1
+		elif ind[i] < ind[j]:
+			normDoc1 = normDoc1 + val[i]**2
+			i=i+1
+		else:
+			dp = dp + val[i]*val[j]
+			normDoc1 = normDoc1 + val[i]**2
+			normDoc2 = normDoc2 + val[j]**2
+			i=i+1
+			j=j+1
+	if dp > 0.0:
+		simi = dp / math.sqrt(normDoc1*normDoc2)
+	else:
+		simi = 0.0
+	cosims[(docNum1,docNum2)] = simi
+	cosims[(docNum2,docNum1)] = simi
+	return simi
+
+
+
+def knnIdx():
+	global ptr
+	global ind
+	global nbrs
+
+	for docnum in range (1,records+1):
+		if docnum in nbrs:
+			continue
+		fr = ptr[docnum-1]
+		to = ptr[docnum]
+		
+		for d in range (fr,to):
+			for x in range(1,len(ptr)):
+				if x in nbrs:
+					continue
+				n = ptr[x-1]
+				if n == fr:
+					continue
+				while(ind[n]<=ind[d] and n<ptr[x]):
+					if ind[n] == ind[d]:
+						nbrs.add(x)
+						nbrs.add(docnum)
+						break
+					n=n+1
+
+def knn(eps,k):
+	knnIdx()
+	print("Neighbours created")
+	global nbrs
+	klist={}
+
+	nbrslist = list(nbrs)
+	l = len(nbrslist)
+	
+	for i in range(0,l):
+		# print(str(i/l)*100)
+		# os.system('clear')
+		for j in range(i+1,l):
+			simi = cosineSimNorm(nbrslist[i],nbrslist[j])
+			# if simi >= 1.0:
+			#  	print("i: " + str(nbrslist[i]) + " j: " + str(nbrslist[j]))
+			if simi >=eps:
+				klist[(nbrslist[i],nbrslist[j])] = simi
+	global numCosineSim
+	print("Number of computed similarities: " + str(numCosineSim))
+	return sorted(klist, key=klist.__getitem__, reverse=True)[:k]
+
+
+
+def runner():
+	#initialize()
+	#printFileStats("wiki1k.csr")
+	createCSR("wiki1k.csr")
+	# for i in range(1,records+1):
+	# 	print(i)
+	# 	for j in range(i,records+1):
+	# 		cosineSimNorm(i,j)
+			# if y > 1.0:
+			# 	print ("i: " + str(i) + " j: " + str(j))
+	#knnIdx()
+	knn(0.9,10)
+	#print (normValue(3))
+	#print("norvalue 3:" + str(normValues[3]))
+	#print(cosineSimNorm(258,259))
+
+runner()
+
+print("--- %s seconds ---" % (time.time() - startTime))
